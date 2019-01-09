649d4970626dedb514440525c6ff7eb756a00677
lhgoulart <lhgoulart92@yahoo.com.br>
2019-01-09 15:37:02 -0200

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 		
 		System.out.println("--- Teste 1: seller findById ---");
-		Seller seller = sellerDAO.findById(3);
+		Seller seller = sellerDAO.findById(1);
 		System.out.println(seller);
 		
 		System.out.println("\n--- Teste 2: seller findByDepartment ---");
@@ -30,11 +30,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n--- Teste 4: seller insert ---");
-		Seller newSeller = new Seller(null, "Tom", "tom@gmail.com", new Date(), 4000.0, new Department(2,null));
-		sellerDAO.insert(newSeller);
-		System.out.println("New Id: " + newSeller.getId());
+//		System.out.println("\n--- Teste 4: seller insert ---");
+//		Seller newSeller = new Seller(null, "Tom", "tom@gmail.com", new Date(), 4000.0, new Department(2,null));
+//		sellerDAO.insert(newSeller);
+//		System.out.println("New Id: " + newSeller.getId());
 		
+		
+		System.out.println("\n--- Teste 5: seller update ---");
+		seller = sellerDAO.findById(1);
+		seller.setName("Joaozinho");
+		sellerDAO.update(seller);
+		System.out.println("Update completed");
 	} // end main
 
 	
